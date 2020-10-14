8c296caba594f268fcb6ccac5982de92bf5f312a
Prajilesh N <nprajilesh@gmail.com>
2020-10-14 12:03:37 +0530

    pep8 styling changes

--- tests/unit/test_slack_notification.py
+++ tests/unit/test_slack_notification.py
@@ -1,12 +1,11 @@
 import os
 import unittest
-from mock import patch, Mock
-import sys
 import mock_import
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/slackNotification')))
+from mock import patch, Mock
 from requests.exceptions import HTTPError
-import slack_notification
+
+from src.slackNotification import slack_notification
 
 
 class TestSlackNotification(unittest.TestCase):
@@ -23,39 +22,43 @@ class TestSlackNotification(unittest.TestCase):
         mock_resp.status_code = status
         return mock_resp
 
-    @patch("slack_notification.requests")
+    @patch("src.slackNotification.slack_notification.requests")
     def test_send_to_slack_response_success(self, mock_requests):
         mock_response = self._mock_response()
         mock_requests.post.return_value = mock_response
         slack_notification.send_to_slack(["webhook1", "webhook2"], {})
         self.assertEqual(mock_response.raise_for_status.call_count, 2)
 
-    @patch("slack_notification.requests")
+    @patch("src.slackNotification.slack_notification.requests")
     def test_send_to_slack_response_failure(self, mock_requests):
-        mock_response = self._mock_response(status=500, raise_for_status=HTTPError("Invalid Url"))
+        mock_response = self._mock_response(status=500,
+                                            raise_for_status=HTTPError(
+                                                "Invalid Url"))
         mock_requests.post.return_value = mock_response
-        self.assertRaises(HTTPError, slack_notification.send_to_slack, ["webhook1"], {})
+        self.assertRaises(HTTPError, slack_notification.send_to_slack,
+                          ["webhook1"], {})
 
-    @patch("slack_notification.requests")
+    @patch("src.slackNotification.slack_notification.requests")
     def test_send_to_slack_no_webhooks(self, mock_requests):
         mock_response = self._mock_response()
         mock_requests.post.return_value = mock_response
         slack_notification.send_to_slack([], {})
         self.assertFalse(mock_response.raise_for_status.called)
 
-    @patch("slack_notification.send_to_slack")
+    @patch("src.slackNotification.slack_notification.send_to_slack")
     def test_lambda_handler_success(self, mock_send_to_stack):
         os.environ["SLACK_WEBHOOK"] = "webhook1,webhook2"
-        event = {"status": "status", "taskname" : "taskname"}
+        event = {"status": "status", "taskname": "taskname"}
         slack_notification.lambda_handler(event, {})
         expected_message = {"Error": "tasknameError", "Cause": "status"}
-        mock_send_to_stack.assert_called_once_with(["webhook1","webhook2"], expected_message)
+        mock_send_to_stack.assert_called_once_with(["webhook1", "webhook2"],
+                                                   expected_message)
 
-    @patch("slack_notification.send_to_slack")
+    @patch("src.slackNotification.slack_notification.send_to_slack")
     def test_lambda_handler_Error(self, mock_send_to_stack):
         os.environ["SLACK_WEBHOOK"] = "webhook1,webhook2,webhook3"
         event = {"Error": "Error", "Cause": "Error-Cause"}
         slack_notification.lambda_handler(event, {})
         expected_message = {"Error": "Error", "Cause": "Error-Cause"}
-        mock_send_to_stack.assert_called_once_with(["webhook1", "webhook2","webhook3"], expected_message)
-
+        mock_send_to_stack.assert_called_once_with(
+            ["webhook1", "webhook2", "webhook3"], expected_message)
