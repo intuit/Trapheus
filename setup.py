ec5047a5599f6823d9faeb8ce14c8db733847b98
rkumar36 <Rohit_Kumar@intuit.com>
2021-06-01 01:18:41 +0530

    Creating setup py for putting in PyPi

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='trapheus', version='0.1.2', author='stationeros (Rohit Kumar)',
+      author_email='rite2rohit88@gmail.com',
+      url='https://github.com/intuit/Trapheus',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3.7',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: System :: Archiving :: Backup',
+          'Topic :: System :: Clustering',
+          'Topic :: System :: Distributed Computing'
+      ],
+      keywords='aws backup lambda rds snapshots step-functions state-machine aws-lambda-layer aws-lambda aws',
+      packages=['trapheus'],
+      install_requires=['boto3', 'aws-sam-cli'],
+      python_requires='>=3.7',
+      description='Backup and restore manager for AWS RDS instances'
+      )
