3d5d924c2991f18e5ac02ff015133851835a01d2
Rohit Kumar <Rohit_Kumar@intuit.com>
2023-09-17 15:39:22 +0530

    Code generation using the LLM

--- labs/DataDialogue/llms/baseline.py
+++ labs/DataDialogue/llms/baseline.py
@@ -10,3 +10,19 @@ def load_local_llm():
 
     return model, tokenizer
 
+def create_source_code(prompt):
+    model, tokenizer = load_local_llm()
+    system_prompt = "<|system|>\nBelow is a conversation between a human and a coding model in Trapheus.<|end|>\n"
+    user_prompt = f"<|user|>\n{prompt}<|end|>\n"
+    assistant_prompt = "<|assistant|>"
+    complete_prompt = system_prompt + user_prompt + assistant_prompt
+    inputs = tokenizer.encode(complete_prompt, return_tensors="pt")
+    outputs = model.generate(inputs,eos_token_id = 0,pad_token_id = 0,
+                             max_length=256,
+                             early_stopping=True)
+    lex =  tokenizer.decode(outputs[0])
+    lex = lex[len(complete_prompt):]
+    if "<|end|>" in lex:
+        cutoff = lex.find("<|end|>")
+        lex = lex[:cutoff]
+    return lex
