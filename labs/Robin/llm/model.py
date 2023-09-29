50711554ee0dcd772db6dda52b45c32a305accd6
Rohit Kumar <Rohit_Kumar@intuit.com>
2023-09-29 15:31:24 +0530

    Refactor

--- labs/Robin/llm/model.py
+++ labs/Robin/llm/model.py
@@ -3,12 +3,14 @@ from typing import Tuple, List
 from llm.prompts import Prompt
 from dataclasses import asdict
 
-def ask_model(discussions: List[Prompt]) -> Tuple[str, List[Prompt]]:
-    # Future scope to incldue model as a parameter as well as load a local
-    # quantized llm
+
+def ask_foundational_model(discourses: List[Prompt]) -> Tuple[str, List[Prompt]]:
+    # TODO Add the foundational language model as a param either local or a hosted one providing users with options to
+    # choose one for privacy or other reasons.
     result = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
-        messages = [asdict(d) for d in discussions]
+        messages = [asdict(discourse) for discourse in discourses]
     )
-    msg = Prompt(**result["choices"][0]["message"])
-    return msg.content, discussions + [msg]
+
+    answer = Prompt(**result["choices"][0]["message"])
+    return answer.content, discourses + [answer]
