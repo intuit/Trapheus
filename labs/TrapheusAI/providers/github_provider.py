4e5b22c34661d4fb496ef32c7f365ce2353ba8af
Rohit Kumar <Rohit_Kumar@intuit.com>
2023-09-30 18:50:17 +0530

    Implementing base provider for github

--- labs/TrapheusAI/providers/github_provider.py
+++ labs/TrapheusAI/providers/github_provider.py
@@ -1,34 +1,40 @@
 import os
 import requests
 import constants
+from baseProvider import IBaseProvider
 
 """
 Searches csv data sets from github
 """
-def search_code(query):
-    github_params = {
-        "q": f"{query} .csv in:path",
-        "per_page": 5,
-    }
-    request_headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": "Bearer " + os.environ.get("GITHUB_ACCESS_TOKEN"),
-    }
-    response = requests.get(constants.GITHUB_END_POINT + constants.GITHUB_SEARCH_PATH, params=github_params,
-                            headers=request_headers)
-    response.raise_for_status()
-    data = response.json()
-    return data
 
-"""
-Returns the formatted repository name, path and url
-"""
-def format_results(results):
-    formatted_results = []
-    for result in results:
-        repo = result["repository"]["full_name"]
-        repo_path = result["path"]
-        repo_url = result["html_url"].replace("/blob/", "/raw/")
+class Github(IBaseProvider):
+
+    def query(self):
+        request_params = {
+            "q": f"{query} .csv in:path",
+            "per_page": 5,
+        }
+        request_headers = {
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": "Bearer " + os.environ.get("GITHUB_ACCESS_TOKEN"),
+        }
+        return request_headers, request_params
+
+    def query(self):
+        request_headers, request_params = self.query()
+        response = requests.get(constants.GITHUB_END_POINT + constants.GITHUB_SEARCH_PATH, params=request_params,
+                                headers=request_headers)
+        response.raise_for_status()
+        response_body = response.json()
+        return response_body
+
+    def extract_data(self):
+        formatted_results = []
+        data = self.query()
+        for result in data:
+            repo = result["repository"]["full_name"]
+            repo_path = result["path"]
+            repo_url = result["html_url"].replace("/blob/", "/raw/")
 
         #defer the response body using streamand inspect only headers
         response = requests.get(repo_url, stream=True)
@@ -51,5 +57,5 @@ def format_results(results):
             "text": content,
         }
         formatted_results.append(result)
+        return formatted_results
 
-    return formatted_results
