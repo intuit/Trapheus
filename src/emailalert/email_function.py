efdaa61bf742b5258b19b3b87f4ba461aecae270
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-01 20:32:28 +0000

    update html body

--- src/emailalert/email_function.py
+++ src/emailalert/email_function.py
@@ -21,7 +21,10 @@ def lambda_handler(event, context):
     <head></head>
     <body>
     <h1>Failure alert for RDS Restore Pipeline</h1>
-    <p>""" + result['failed step'] + """ : """ + result[constants.CAUSE] + """</p>
+    <p>database id:""" + result['database id'] """</p>
+    <p>snapshot id:""" + result['snapshot id'] """</p>
+    <p>failed step:""" + result['taskname'] """</p>
+    <p>cause of failure:""" + result[constants.CAUSE] """</p>
     </body>
     </html>"""
 
