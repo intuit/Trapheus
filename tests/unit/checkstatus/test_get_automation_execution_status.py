fed8fd8e6e7b0a5fe574416c969d6f9521d04f61
namitad <devadas.namita@gmail.com>
2024-02-10 22:56:01 +0530

    adding tests to get_automation_execution_status.py

--- tests/unit/checkstatus/test_get_automation_execution_status.py
+++ tests/unit/checkstatus/test_get_automation_execution_status.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src/checkstatus')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src/common/python')))
@@ -17,12 +18,12 @@ class ContextManager:
     def get_remaining_time_in_millis(self):
         return --self.seconds * 1000
 
+
 @patch("checkstatus.get_automation_execution_status.boto3.client")
 class TestResourceProvider(unittest.TestCase):
     def setUp(self):
         self.mock_waiter = Mock()
         self.task_complete = "TASK_COMPLETE"
-        self.task_failed = "TASK_FAILED"
         self.mock_automation_not_found_failure = custom_exceptions.SSMAutomationExecutionException(
             "Specified value for ExecutionId is not valid")
         self.mock_automation_execution_failure = custom_exceptions.SSMAutomationExecutionException(
@@ -37,20 +38,6 @@ class TestResourceProvider(unittest.TestCase):
                     "DocumentName": "trapheus",
                     "DocumentVersion": "2",
                     "AutomationExecutionStatus": "Success"
-                }
-            ]
-        }
-
-        self.mocked_describe_automation_execution_failure = {
-            "ResponseMetadata": {
-                'HTTPStatusCode': 200
-            },
-            "AutomationExecutionMetadataList": [
-                {
-                    "AutomationExecutionId": "execution-123",
-                    "DocumentName": "trapheus",
-                    "DocumentVersion": "2",
-                    "AutomationExecutionStatus": "Failed"
                 }
             ]
         }
@@ -70,34 +57,38 @@ class TestResourceProvider(unittest.TestCase):
         self.assertEqual(data.get("taskname"), "ShareSnapshot")
         self.assertEqual(data.get("task"), self.task_complete)
 
-    @patch('checkstatus.get_automation_execution_status.SSMAutomationExecutionStatusWaiter')
-    def test_get_automation_execution_status_failure(self, mock_waiter_patch, mock_client):
-        # # test failure for automation execution
-        mock_ssm = mock_client.return_value
-        mock_ssm.describe_automation_executions.return_value = self.mocked_describe_automation_execution_failure
-        mock_waiter_client = mock_waiter_patch.start()
-        mock_waiter = self.mock_waiter
-        # mock_waiter.wait.side_effect = Exception("Waiter encountered a terminal failure state")
-        mock_waiter_client.check_automation_execution_status.return_value = mock_waiter
-        mock_ssm.get_waiter.return_value = mock_waiter
+    @patch('checkstatus.get_automation_execution_status.SSMAutomationExecutionStatusWaiter',
+           'check_automation_execution_status')
+    def test_get_automation_execution_status_failure(self, mock_ssm_waiter_patch):
+        # test failure for automation execution
+        mock_ssm_waiter_client = mock_ssm_waiter_patch.start()
+        mock_ssm_waiter_client.side_effect = Exception("Waiter encountered a terminal failure state")
         event = {"output": {"automation_execution_id": "execution-123", "taskname": "abc"}}
         context = ContextManager(45)
-        with self.assertRaises(Exception) as err:
+        with pytest.raises(Exception) as err:
             _ = get_automation_execution_status.lambda_get_automation_execution_status(event, context)
             self.assertEqual(err.exception, self.mock_automation_execution_failure)
 
-    @patch('checkstatus.get_automation_execution_status.SSMAutomationExecutionStatusWaiter')
-    def test_get_automation_execution_status_automation_not_found(self, mock_waiter_patch, mock_client):
+    @patch('checkstatus.get_automation_execution_status.SSMAutomationExecutionStatusWaiter',
+           'check_automation_execution_status')
+    def test_get_automation_execution_status_automation_not_found(self, mock_waiter_patch):
         # test automation id not found exception while checking completion of the automation execution
-        mock_waiter = self.mock_waiter
-        mock_ssm = mock_client.return_value
         mock_waiter_client = mock_waiter_patch.start()
-        mock_waiter_client.describe_automation_executions.side_effect = Exception(
+        mock_waiter_client.return_value = Exception(
             "Specified value for ExecutionId is not valid")
-        mock_waiter_client.check_automation_execution_status.return_value = mock_waiter
-        mock_ssm.get_waiter.return_value = mock_waiter
         event = {"output": {"automation_execution_id": "execution-124", "taskname": "abc"}}
         context = ContextManager(25)
-        with self.assertRaises(Exception) as err:
+        with pytest.raises(custom_exceptions.SSMAutomationExecutionException) as err:
             _ = get_automation_execution_status.lambda_get_automation_execution_status(event, context)
             self.assertEqual(err.exception, self.mock_automation_not_found_failure)
+
+    @patch('checkstatus.get_automation_execution_status.SSMAutomationExecutionStatusWaiter',
+           'check_automation_execution_status')
+    def test_get_automation_execution_status_waiter_not_initialised(self, mock_waiter_patch):
+        mock_waiter_client = mock_waiter_patch.start()
+        mock_waiter_client.side_effect = Exception(
+            "Waiter could not be initialised")
+        event = {"output": {"automation_execution_id": "execution-124", "taskname": "abc"}}
+        context = ContextManager(25)
+        with pytest.raises(custom_exceptions.SSMAutomationExecutionException) as err:
+            _ = get_automation_execution_status.lambda_get_automation_execution_status(event, context)
